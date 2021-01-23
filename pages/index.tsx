15ea0e979e2e8462b64cff7c595e9f2f02ed2509
Ramachandran Gunasekaran <pingme@ramachandrang.info>
2021-01-23 20:53:33 +0530

    Adding Styled component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
+import styled, { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+ h1 {
+   font-size: 4rem;
+ }
+`;
 
-const IndexPage = () => <Layout>hi</Layout>;
+const Container = styled.div`
+  text-align: center;
+`;
+
+const IndexPage = () => (
+  <Layout>
+    <Container>
+      <GlobalStyle />
+      <h1>Hello, world!</h1>
+    </Container>
+  </Layout>
+);
 
 export default IndexPage;
